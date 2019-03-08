9cbb99690d97fd825df98f79656f11687a3b66ee
J-Kyu <wjstkdrb91@naver.com>
2019-03-09 01:45:31 +0900

    Init Commit

---
+++ Assets/DOTEManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DOTEManager : MonoBehaviour {
+
+	// Use this for initialization
+	[SerializeField] private GameObject gameOverUI = null;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+
+	public void Retry(){
+		//장애물 새로 만들기
+		//원위치
+		gameOverUI.SetActive(false);
+
+	}
+
+	public void BackToMenu(){
+		//Menu 로 돌아가기....maybe Scene 바꾸기
+		gameOverUI.SetActive(false);
+
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    private float m_Speed = 0.1f;
+	[SerializeField]private GameObject gameOver = null;
+    // Use this for initialization
+    void Start() {
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        //keyboaard 값을 입력하여 움직이는 것
+		if(this.transform.position.y<-1){
+			GameOver();
+		}
+        Move();
+
+
+
+    }
+
+    private void Move() {
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Translate(new Vector3(0, 0, 1) * m_Speed);
+        } 
+		if (Input.GetKey(KeyCode.A)) {
+            transform.Translate(new Vector3(-1, 0, 0) * m_Speed);
+
+        }
+		if (Input.GetKey(KeyCode.D)) {
+            transform.Translate(new Vector3(1, 0, 0) * m_Speed);
+
+        }
+		if (Input.GetKey(KeyCode.S)) {
+            transform.Translate(new Vector3(0, 0, -1) * m_Speed);
+
+        }
+    }
+
+	void OnCollisionEnter(Collision other) {
+        if (other.gameObject.CompareTag("Enemy") ) {
+            GameOver();
+			//game oVer
+
+        }
+		
+    }
+
+	public void GameOver(){
+	
+			gameOver.SetActive(true);
+		
+	}
+
+}
