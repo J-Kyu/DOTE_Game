7833c80880d1f1e7cd71d2bd00de2217f3ef76ae
J-Kyu <wjstkdrb91@naver.com>
2019-03-09 02:32:54 +0900

    - Complete Zone 제작

---
+++ Assets/CompleteCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompleteCube : MonoBehaviour {
+
+	[SerializeField] private GameObject nextStageUI =  null;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.CompareTag("Player")){
+			Debug.Log("Game Comeplete");
+			nextStageUI.gameObject.SetActive(true);
+		}
+	}
+}
